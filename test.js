52c515ed4c5f8ea3ef1fbdc73753c19aca4d072c
Ferîşte-Beep-Boop <angel.munoz692003@gmail.com>
2024-07-21 12:00:37 +0200

    bouton test

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 let boutonTest = document.createElement("button");
+boutonTest.textContent = "bouton test";
 let divTest = document.querySelector("#conteneurTest");
 
 boutonTest.addEventListener('change', function() {
